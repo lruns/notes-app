467af2686e0460d92aa72834d096a56f9587bd29
Andrey Plahtiy <aplaxtiy@gmail.com>
2024-08-23 17:25:57 +0300

    optimization

--- src/components/note_container/note_container.tsx
+++ src/components/note_container/note_container.tsx
@@ -2,25 +2,35 @@ import styles from "./note_container.module.css"
 import {observer} from "mobx-react-lite";
 import {useStore} from "../../store/app_store_context.ts";
 
-const NoteContainer = observer(() => {
 
-    const {noteStore, editorStore} = useStore()
+const NoteContainer = observer(() => {
+    const {editorStore} = useStore()
 
     return (
         <div className={styles.container}>
             <button className={styles.addNewNote} onClick={() => editorStore.startNewNote()}>
                 Add new note
             </button>
-            {noteStore.notes.map(note => (
-            <article className={styles.noteWrapper} key={note.id}>
-                <h3>{note.title}</h3>
-                <p>{note.content}</p>
-                <button className={styles.editNote} onClick={() => editorStore.startEditNote(note)}>
-                    Edit note
-                </button>
-            </article>
-            ))}
+            <Notes/>
         </div>
+    )
+});
+
+const Notes = observer(() => {
+    const {noteStore, editorStore} = useStore()
+
+    return (
+        <>
+            {noteStore.notes.map(note => (
+                <article className={styles.noteWrapper} key={note.id}>
+                    <h3>{note.title}</h3>
+                    <p>{note.content}</p>
+                    <button className={styles.editNote} onClick={() => editorStore.startEditNote(note)}>
+                        Edit note
+                    </button>
+                </article>
+            ))}
+        </>
     )
 });
 
