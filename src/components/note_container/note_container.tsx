d88175c3d18463aa7f1fdae868188a0da40f4ed4
Andrey Plahtiy <aplaxtiy@gmail.com>
2024-08-25 19:08:34 +0300

    Redesign and some refactoring

--- src/components/note_container/note_container.tsx
+++ src/components/note_container/note_container.tsx
@@ -1,36 +1,44 @@
 import styles from "./note_container.module.css"
 import {observer} from "mobx-react-lite";
 import {useStore} from "../../store/app_store_context.ts";
-
+import {useCallback} from "react";
 
 const NoteContainer = observer(() => {
-    const {editorStore} = useStore()
+    const {noteStore, editorStore} = useStore()
 
     return (
         <div className={styles.container}>
             <button className={styles.addNewNote} onClick={() => editorStore.startNewNote()}>
-                Add new note
+                Создать новую заметку
             </button>
-            <Notes/>
+            {noteStore.notes.map(note => (
+                <NoteComponent note={note}/>
+            ))}
         </div>
     )
 });
 
-const Notes = observer(() => {
+const NoteComponent = observer(({note}) => {
     const {noteStore, editorStore} = useStore()
 
+    const removeNote = useCallback((id) => {
+        noteStore.removeNote(id);
+        editorStore.stopEdit()
+    }, [editorStore, noteStore])
+
     return (
-        <>
-            {noteStore.notes.map(note => (
-                <article className={styles.noteWrapper} key={note.id}>
-                    <h3>{note.title}</h3>
-                    <p>{note.content}</p>
-                    <button className={styles.editNote} onClick={() => editorStore.startEditNote(note)}>
-                        Edit note
-                    </button>
-                </article>
-            ))}
-        </>
+        <article className={styles.noteWrapper} key={note.id}>
+            <h3>{note.title}</h3>
+            <p>{note.content}</p>
+            <div className={styles.buttonContainer}>
+                <button onClick={() => editorStore.startEditNote(note)}>
+                    Изменить
+                </button>
+                <button onClick={() => removeNote(note.id)}>
+                    Удалить
+                </button>
+            </div>
+        </article>
     )
 });
 
--- src/components/note_editor/note_editor.tsx
+++ src/components/note_editor/note_editor.tsx
@@ -1,8 +1,8 @@
-import NoteForm from "./note_form/note_form.tsx";
 import Modal from "../UI/modal/modal.tsx";
 import {EditorMode} from "../../store/editor_store.ts";
 import {observer} from "mobx-react-lite";
 import {useStore} from "../../store/app_store_context.ts";
+import styles from "./note_editor.module.css";
 
 const NoteEditor = observer(() => {
     const editorStore = useStore().editorStore;
@@ -13,6 +13,31 @@ const NoteEditor = observer(() => {
             <NoteForm/>
         </Modal>
     );
+});
+
+const NoteForm = observer(() => {
+    const {editorStore} = useStore();
+    const note = editorStore.currentNote;
+
+    return (
+        <form className={styles.formContainer}>
+            <p>
+                <label htmlFor="name">Заголовок</label>
+                <br/>
+                <input value={note.title} onChange={e => note.title = e.target.value}
+                       type="text" placeholder="Введите заголовок"/>
+            </p>
+            <p>
+                <label htmlFor="email">Текст заметки</label>
+                <br/>
+                <textarea value={note.content} onChange={e => note.content = e.target.value}
+                          placeholder="Введите текст заметки"/>
+            </p>
+            <button onClick={() => editorStore.submit()}>
+                {editorStore.currentMode === EditorMode.CREATE ? "Создать" : "Обновить"}
+            </button>
+        </form>
+    )
 });
 
 export default NoteEditor;
